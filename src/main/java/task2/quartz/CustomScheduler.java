314d3e21494e1e4121e9b14deaccf30d284da10f
Mikhail Kerko <Mikhail Kerko>
2017-11-07 16:19:12 +0300

    Initial.

---
+++ src/main/java/task2/quartz/CustomScheduler.java
@@ -0,0 +1,36 @@
+package task2.quartz;
+
+import org.apache.log4j.BasicConfigurator;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+/**
+ * Created by Mikhail on 03.11.2017.
+ */
+public class CustomScheduler {
+
+    public static void main(String[] args) throws SchedulerException {
+        BasicConfigurator.configure();
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        JobDetail jobDetail = newJob(EmailValidatorJob.class)
+                .usingJobData("email", "kerkomikhail@gmail.com")
+                .withIdentity("job1", "group1")
+                .build();
+
+        Trigger trigger = newTrigger()
+                .withIdentity("trigger1", "group1")
+                .startNow()
+                .withSchedule(simpleSchedule().withIntervalInSeconds(2).repeatForever())
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+        scheduler.start();
+    }
+}
---
+++ src/main/java/task2/quartz/EmailValidatorJob.java
@@ -0,0 +1,40 @@
+package task2.quartz;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import task2.quartz.util.Util;
+
+import java.io.IOException;
+
+/**
+ * Created by Mikhail on 03.11.2017.
+ */
+public class EmailValidatorJob implements Job{
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(Util.NAME);
+        factory.setPassword(Util.PASSWORD);
+        factory.setVirtualHost(Util.VHOST);
+        factory.setHost(Util.HOST);
+        factory.setPort(Util.PORT);
+        try {
+            Connection conn = factory.newConnection();
+            Channel channel = conn.createChannel();
+            String exchangeName = "myExchange";
+            String routingKey = "testRoute";
+
+            byte[] messageBodyBytes = (context.getJobDetail().getJobDataMap().getString("email")).getBytes();
+            channel.basicPublish(exchangeName, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, messageBodyBytes);
+            channel.close();
+            conn.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
